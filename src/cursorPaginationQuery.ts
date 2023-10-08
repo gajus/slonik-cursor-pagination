36f709cd93e6ff807a33145797e2233f3039866f
Gajus Kuizinas <gajus@gajus.com>
2023-10-07 19:41:55 -0600

    fix: correct logic of hasPreviousPage

--- src/cursorPaginationQuery.ts
+++ src/cursorPaginationQuery.ts
@@ -39,15 +39,23 @@ export const cursorPaginationQuery = async <T extends ZodTypeAny>(
     }),
   );
 
-  const previousRows = await pool.any(
-    createCursorPaginationQuery({
-      after: before ? fromCursor(before) : undefined,
-      before: after ? fromCursor(after) : undefined,
-      first: 1,
-      orderBy,
-      query,
-    }),
-  );
+  let hasPreviousPage: boolean;
+
+  if (!after && !before) {
+    hasPreviousPage = false;
+  } else {
+    const previousRows = await pool.any(
+      createCursorPaginationQuery({
+        after: before ? fromCursor(before) : undefined,
+        before: after ? fromCursor(after) : undefined,
+        first: 1,
+        orderBy,
+        query,
+      }),
+    );
+
+    hasPreviousPage = previousRows.length > 0;
+  }
 
   const organizedRows = before
     ? rows.slice(0, first).reverse()
@@ -59,7 +67,7 @@ export const cursorPaginationQuery = async <T extends ZodTypeAny>(
         ? toCursor(organizedRows[organizedRows.length - 1])
         : null,
       hasNextPage: rows.length === first + 1,
-      hasPreviousPage: previousRows.length === 1,
+      hasPreviousPage,
       startCursor: organizedRows[0] ? toCursor(organizedRows[0]) : null,
     },
     rows: organizedRows,
